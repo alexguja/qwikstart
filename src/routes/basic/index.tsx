04555a8b00d8c0ff49931c544e5d8ba941a84fe8
alexguja <al.guja@outlook.com>
2024-11-29 23:55:51 +0100

    add fetching example

--- src/routes/basic/index.tsx
+++ src/routes/basic/index.tsx
@@ -1,4 +1,4 @@
-import { component$, useStore } from "@builder.io/qwik";
+import { component$, useStore, Resource, useResource$ } from "@builder.io/qwik";
 
 export default component$(() => {
   // useStore is used to
@@ -9,30 +9,66 @@ export default component$(() => {
   //   The subscriptions are then used to automatically update the component template if a store changes.
   const github = useStore({
     org: "QwikDev",
-    repos: ["qwik", "partytown"] as string[] | null,
+  });
+
+  const reposResource = useResource$<string[]>(({ track, cleanup }) => {
+    // We need a way to re-run fetching data whenever the `github.org` changes.
+    // Use `track` to trigger re-running of this data fetching function.
+    track(() => github.org);
+
+    // A good practice is to use `AbortController` to abort the fetching of data if
+    // new request comes in. We create a new `AbortController` and register a `cleanup`
+    // function which is called when this function re-runs.
+    const controller = new AbortController();
+    cleanup(() => controller.abort());
+
+    // Fetch the data and return the promises.
+    return getRepositories(github.org, controller);
   });
 
   return (
     <main>
       <p>
         <label>
-          GitHub organization:
+          GitHub username:
           <input
-            value="github.org"
-            onInput$={(ev, el) => (github.org = el.value)} // Update the store when the input changes.
+            value={github.org}
+            onInput$={(ev, el) => (github.org = el.value)}
           />
         </label>
       </p>
       <section>
-        <ul>
-          <li>
-            <a href="https://github.com/QwikDev/qwik">Qwik</a>
-          </li>
-          <li>
-            <a href="https://github.com/BuilderIO/partytown">Partytown</a>
-          </li>
-        </ul>
+        <Resource
+          value={reposResource}
+          onPending={() => <>Loading...</>}
+          onRejected={(error) => <>Error: {error.message}</>}
+          onResolved={(repos) => (
+            <ul>
+              {repos.map((repo) => (
+                <li>
+                  <a href={`https://github.com/${github.org}/${repo}`}>
+                    {repo}
+                  </a>
+                </li>
+              ))}
+            </ul>
+          )}
+        />
       </section>
     </main>
   );
 });
+
+export async function getRepositories(
+  username: string,
+  controller?: AbortController
+): Promise<string[]> {
+  const resp = await fetch(`https://api.github.com/users/${username}/repos`, {
+    signal: controller?.signal,
+  });
+
+  const json = await resp.json();
+  return Array.isArray(json)
+    ? json.map((repo: { name: string }) => repo.name)
+    : Promise.reject(json);
+}
